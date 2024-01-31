feb6d90dfa483ae575f2be75b71bccd2d75c10a4
Tarik IRHBOULA <tarik.irhboula@gmail.com>
2024-01-31 12:08:02 +0100

    theme, copilot, autosave and neorg

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,49 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "gptlang/CopilotChat.nvim",
+    cmd = "Copilot",
+    event = "User AstroFile",
+  },
+  {
+    "pocco81/auto-save.nvim",
+    event = "BufEnter",
+  },
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.summary"] = {},
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {
+            config = {
+              folds = true,
+              icon_preset = "diamond",
+              icons = {
+                todo = {
+                  pending = {
+                    icon = "⏱"
+                  },
+                }
+              },
+            },
+          }, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                work = "~/notes/work",
+                home = "~/notes/home",
+              },
+              default_workspace = "home"
+            },
+          },
+        },
+      }
+    end,
+  }
 }
